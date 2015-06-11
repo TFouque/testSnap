f1e3b8e23750e7be4f542bb6943923482690e25e
TFouque <thomas.fouque@outlook.fr>
2015-06-11 21:22:28 +0200

    CAAAAAMMMEEERRRAAAAA QUUUIII MAAAARCCCHHHEEE

--- src/com/example/testsnap/MessagerieActivity.java
+++ src/com/example/testsnap/MessagerieActivity.java
@@ -13,6 +13,7 @@ public class MessagerieActivity extends ListActivity {
 
     /** Called when the activity is first created. */
     @Override
+    
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_messagerie);
